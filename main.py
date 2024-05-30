271122461a3bc53f5aae547002ddc72a8b043c78
Ayushp051 <anpatel051@gmail.com>
2024-05-30 11:29:16 -0400

    changed text color

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import os
 from dotenv import load_dotenv
 load_dotenv()
-st.title("Weather Website")
+st.title("Weather Website🌤️")
 base_url = "http://api.weatherapi.com/v1/current.json"
 key = key = os.getenv("WEATHER_API_KEY")
 location = st.text_input("Please enter the city name")
@@ -13,8 +13,11 @@ response = requests.get(request_url)
 if response.status_code == 200:
     weather_data = response.json()
     print(weather_data)
-    st.subheader(f"You are viewing weather for: :green[{weather_data['location']['name']}, {weather_data['location']['region']}, {weather_data['location']['country']}]")
-    st.write(f"The local time is {weather_data['location']['localtime']}")
+    
+    st.subheader("You are viewing weather for:")
+    st.subheader(f":green[{weather_data['location']['name']}], :green[{weather_data['location']['region']}], :green[{weather_data['location']['country']}]")
+    st.write(f"The local time is :violet[{weather_data['location']['localtime']}]")
+    st.write(f"Coordinates: :violet[{weather_data['location']['lat']}] lat, :violet[{weather_data['location']['lon']}] lon")
 
     col1, col2,col3 = st.columns(3)
     with col1:
